d2728d05ea7d3ef4fdc58f4e4877f66596acd4e9
linus <lagbleze@st.ug.edu.gh>
2026-09-04 23:36:50 +0200

    fix: update

--- audio_analysis.py
+++ audio_analysis.py
@@ -6,6 +6,15 @@ def inference_audio(audio_waveform, sampling_rate,
                     max_frames=32,
                     model_id="Qwen/Qwen2.5-Omni-7B",
                     ):
-    messages = [{"role": "system", "content": []}, 
-                {"role": "user", "content": []}
+    messages = [{"role": "system", "content": [{"type": "text", "text": sys_prompt}]}, 
+                {"role": "user", "content": [{"type": "audio", "audio": audio_waveform, "sampling_rate": sampling_rate},
+                                             {"type": "text", "text": prompt}
+                                            ]
+                 }
                 ]
+    text = processor.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+    audios, images, videos = process_mm_info(messages, use_audio_in_video=False)
+    
+    inputs = processor(text=text, audio=audios, images=images, videos=videos,
+                       return_tensors="pt", padding=True, use_audio_in_video=False).to(model.device))
+    
