58669fc17d7f8ce15ec89f8768dea9fcb37c2a37
linus <lagbleze@st.ug.edu.gh>
2026-09-18 23:58:30 +0200

    update

--- distributed_train.py
+++ distributed_train.py
@@ -4,11 +4,22 @@ import os
 import torch
 import numpy as np
 from datasets import load_dataset
-from transformers import AutoTokenizer, AutoModelForSequenceClassification, TrainingArguuments, Trainer
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, TrainingArguments, Trainer
 
 import evaluate
 from huggingface_hub import notebook_login
 from ray.train import RunConfig, ScalingConfig, CheckpointConfig, Checkpoint
-from ray.huggingface.transformers import prepare_trainer, RayTrainReportCallback
+from ray.train.huggingface.transformers import prepare_trainer, RayTrainReportCallback
 from ray.train.torch import TorchTrainer
+from ray import tune
+from ray.tune import Tuner
+from ray.tune.schedulers.async_hyperband import ASHAScheduler
+
 # %%
+use_gpu = True
+num_workers = 1
+# %%
+wandb.login(key="")
+
+#%%
+task = "cb"
